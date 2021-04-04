a74663addadedf081c5a207f47fd25ef460738ae
Holar <vitalii.hurianov@gmail.com>
2021-04-05 00:07:17 +0300

    Rewrote everything to be more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,121 +1,113 @@
-use std::cmp::Ordering;
+use dont_disappear;
 use std::env;
 use std::fs;
-use dont_disappear;
 
+enum Direction {
+    Left,
+    Right,
+    Stay,
+}
 struct Instruction {
-	from_char: char,
-	from_state: usize,
-	to_char: char,
-	to_state: String,
-	direction: char,
+    current_char: Option<char>,
+    current_state: usize,
+    new_char: Option<char>,
+    new_state: Option<usize>,
+    new_direction: Direction,
 }
 
 fn main() {
-	println!("Turing machine converter by Vitalii Hurianov (@not_holar).");
+    println!("Turing machine converter by Vitalii Hurianov (@not_holar).");
 
-	let args: Vec<String> = env::args().collect();
+    let path = env::args()
+        .skip(1)
+        .next()
+        .unwrap_or_else(|| "input.txt".to_string());
 
-	let path = match args.len().cmp(&2) {
-		Ordering::Less => "input.txt",
-		_ => &args[1],
-	};
+    let input = fs::read_to_string(&path).unwrap_or_else(|_| {
+        println!("Failed to read the file ({}).", &path);
+        println!();
+        println!("Usage:");
+        println!("   a) drag and drop a table TM .txt file on the executable");
+        println!("   b) CLI: \"converter [PATH]\" e.g. \"converter input.txt\"");
+        println!(
+            "   c) put a file named input.txt in the same directory as the executable and run it"
+        );
+        println!();
 
-	let input = match fs::read_to_string(path) {
-		Ok(input) => input,
-		Err(_) => {
-			println!("Failed to read the file ({}).", path);
-			println!();
-			println!("Usage:");
-			println!("   a) drag and drop a table TM .txt file on the executable");
-			println!("   b) CLI: \"converter [PATH]\" e.g. \"converter input.txt\"");
-			println!("   c) put a file named input.txt in the same directory as the executable and run it");
-			println!();
+        dont_disappear::any_key_to_continue::default();
 
-			dont_disappear::any_key_to_continue::default();
+        panic!();
+    });
 
-			panic!();
-		}
-	};
+    let parsed_instructions: Vec<Instruction> = input
+        .lines()
+        .filter(|line| line.len() != 0)
+        .map(|line| {
+            let current_char = match line.chars().next().unwrap() {
+                ' ' => None,
+                x => Some(x),
+            };
 
-	let mut instructions: Vec<Instruction> = Vec::new();
+            line.split('\t')
+                .skip(1)
+                .enumerate()
+                .filter(|(_, instruction)| instruction.len() > 2)
+                .map(|(current_state, instruction)| {
+                    let current_state = current_state + 1;
 
-	for line in input.lines() {
-		let mut line: Vec<char> = line.chars().collect();
+                    let new_char = match instruction.chars().nth(0).unwrap() {
+                        '_' => None,
+                        x => Some(x),
+                    };
 
-		line.push('\t');
+                    let new_direction = match instruction.chars().nth(1).unwrap() {
+                        '<' => Direction::Left,
+                        '>' => Direction::Right,
+                        _ => Direction::Stay,
+                    };
 
-		let from_char = line[0];
-		let mut from_state = 0;
-		let mut to_char = ' ';
-		let mut to_state = String::new();
-		let mut direction = 'S';
+                    let new_state = match instruction[2..].parse().unwrap() {
+                        0 => None,
+                        x => Some(x),
+                    };
 
-		let mut on_state: bool = false;
+                    Instruction {
+                        current_char,
+                        current_state,
+                        new_char,
+                        new_state,
+                        new_direction,
+                    }
+                })
+                .collect::<Vec<Instruction>>()
+        })
+        .flatten()
+        .collect();
 
-		for ch in &line[2..] {
-			match ch {
-				'\t' => {
-					from_state += 1;
+    let output = parsed_instructions
+        .iter()
+        .map(|instruction| {
+            format!(
+                "q{},{}:{},{},{};",
+                instruction.current_state,
+                instruction.current_char.unwrap_or(' '),
+                match instruction.new_state {
+                    Some(x) => format!("q{}", x),
+                    None => String::from("!"),
+                },
+                instruction.new_char.unwrap_or(' '),
+                match instruction.new_direction {
+                    Direction::Left => 'L',
+                    Direction::Right => 'R',
+                    Direction::Stay => 'S',
+                }
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n");
 
-					if on_state {
-						let to_state: usize = to_state.parse().unwrap();
-
-						let to_state = match to_state {
-							0 => String::from("!"),
-							_ => format!("q{}", to_state),
-						};
-
-						instructions.push(Instruction {
-							from_char,
-							from_state,
-							to_char,
-							to_state,
-							direction,
-						});
-					}
-
-					to_state = String::new();
-					on_state = false;
-				}
-				'<' | '>' | '.' => {
-					on_state = true;
-
-					direction = match ch {
-						'<' => 'L',
-						'>' => 'R',
-						_ => 'S',
-					}
-				}
-				_ => {
-					if on_state {
-						to_state.push(*ch);
-					} else {
-						to_char = match ch {
-							'_' => ' ',
-							_ => *ch,
-						};
-					}
-				}
-			}
-		}
-	}
-
-	let mut output = String::new();
-
-	for instruction in instructions {
-		output.push_str(&format!(
-			"q{},{}:{},{},{};\n",
-			instruction.from_state,
-			instruction.from_char,
-			instruction.to_state,
-			instruction.to_char,
-			instruction.direction
-		));
-	}
-
-	match fs::write("output.txt", output) {
-		Ok(_) => println!("Successfully written the result to a file."),
-		Err(_) => println!("Failed to write the result to a file."),
-	}
+    match fs::write("output.txt", output) {
+        Ok(_) => println!("Successfully written the result to a file."),
+        Err(_) => println!("Failed to write the result to a file."),
+    }
 }
